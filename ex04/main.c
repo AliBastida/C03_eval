1c59804d86f5f6713777ba5bc8e2826f48692d66
Oscar Gonzalez simon <ogonzale@cbr4s6.42barcelona.com>
2022-04-11 12:37:23 +0200

    modify ex04

--- ex04/main.c
+++ ex04/main.c
@@ -11,18 +11,24 @@ int	main(void)
 	char	*s2_test2_o = "";
 	char	*s1_test3_o = "Foo Bar Baz";
 	char	*s2_test3_o = "Barz";
+	char	*s1_test4_o = "Foo";
+	char	*s2_test4_o = "Barz";
 	char	*s1_test1_e = "Foo Bar Baz";
 	char	*s2_test1_e = "Bar";
 	char	*s1_test2_e = "Foo Bar Baz";
 	char	*s2_test2_e = "";
 	char	*s1_test3_e = "Foo Bar Baz";
 	char	*s2_test3_e = "Barz";
+	char	*s1_test4_e = "Foo";
+	char	*s2_test4_e = "Barz";
 	char	*test1_o;
 	char    *test2_o;
 	char    *test3_o;
+	char	*test4_o;
 	char    *test1_e;
 	char    *test2_e;
 	char    *test3_e;
+	char	*test4_e;
 	
 	printf("\n--------------------------TEST 1------------------------------\n");
 	printf("str: %s \n", s1_test1_e);
@@ -50,5 +56,16 @@ int	main(void)
 	printf("Expected str: %s\n", test3_e);
 	printf("Obtained str: %s\n", test3_o);
 	printf("----------------------------------------------------------------\n");
+	
+	printf("\n--------------------------TEST 4------------------------------\n");
+	printf("str: %s \n", s1_test4_e);
+	printf("to_find: %s \n", s2_test4_e);
+	test4_e = strstr(s1_test4_e, s2_test4_e);
+	test4_o = ft_strstr(s1_test4_o, s2_test4_o);
+	printf("Expected str: %s\n", test4_e);
+	printf("Obtained str: %s\n", test4_o);
+	printf("----------------------------------------------------------------\n");
+	
+
 	return (0);
 }
